513cc6884fbea08a030510236ede125e927d1197
Ank <toprakaysenur96@gmail.com>
2024-12-28 13:18:27 +0300

    first commit

---
+++ toprake-commerce/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // React bileşenlerini tarar
+  theme: {
+    extend: {
+      colors: {
+      },
+      fontFamily: {
+      },
+      spacing: {
+      },
+    },
+  },
+  plugins: [
+  ],
+};
